35c0c95e587e9a2230d5541854c5708dc6af49d7
Cirano <ciranobelardony@gmail.com>
2022-06-28 15:47:31 -0300

    JS

--- Fundamentos Web/assets/scripts/scripts.js
+++ Fundamentos Web/assets/scripts/scripts.js
@@ -1,3 +1,39 @@
-function hello(){
-    alert("é us guri")
+
+
+/*
+JS é Case Sensetive
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: 
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txt = document.querySelector('#txtnome')
+    if(nome.value.length < 3){
+        txt.innerHTML = 'Nome Inválido'
+        txt.style.color = 'red'
+    }else{
+        txt.innerHTML = 'Nome Válido'
+        txt.style.color = 'green'
+    }
+}
+
+function validaEmail() {
+    let txtemail = document.querySelector('#txtemail')
+    if(email.value.indexOf('@')==1){
+        txtemail.innerHTML = "E-mail válido"
+        txtemail.style.color = 'green'
+    }else{
+        txtemail.innerHTML = "E-mail inválido"
+        txtemail.style.color = 'red'
+    }
 }
